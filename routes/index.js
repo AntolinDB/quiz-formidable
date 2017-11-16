abb3961d0b88884b5a44cb53dda2c4a372da7d41
AntolinDB <antolin.diaz@outlook.com>
2017-11-16 18:34:18 +0100

    test

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
 });
 
-router.get('/quizes/question', quizesController.question);
-router.get('/quizes/answer', quizesController.answer);
+// Autoload de comandos con :quizId
+router.param('quizId', quizesController.load); // autoload :quizId
+
+router.get('/quizes', quizesController.index);
+router.get('/quizes/:quizId(\\d+)', quizesController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizesController.answer);
+
+/*router.get('/quizes/question', quizesController.question);
+router.get('/quizes/answer', quizesController.answer);*/
 
 module.exports = router;
