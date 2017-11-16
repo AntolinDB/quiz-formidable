6d9ced87687669a080e17f43b82740c5137c77f0
AntolinDB <antolin.diaz@outlook.com>
2017-11-16 13:16:22 +0100

    Primera pregunta controller y vistas

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+var quizesController = require('../controllers/quizes.controller');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
 });
+
+router.get('/quizes/question', quizesController.question);
+router.get('/quizes/answer', quizesController.answer);
 
 module.exports = router;
