88ead6dd6703a0f569e3d004bd463044490bda0e
AntolinDB <antolin.diaz@outlook.com>
2017-11-16 12:44:16 +0100

    Adaptación primera pagina, favicon y eliminado users
    .

--- app.js
+++ app.js
@@ -1,29 +1,31 @@
+// Importar paquetes con middlewares
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+// Importar enrutadores
 var index = require('./routes/index');
-var users = require('./routes/users');
-
+var routes = require('./routes/index');
+// Crear aplicación
 var app = express();
 
-// view engine setup
+// Configurar motor de vistas e instalar generadore de vistas EJS
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// Instalar middlewares
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+// Instalar enrutadores
+app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
