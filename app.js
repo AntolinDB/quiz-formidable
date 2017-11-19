0969fca407272ac56e30bf7f31fbfd3a3e5133b9
Antolin Diaz <antolin.diaz@outlook.com>
2017-11-19 01:36:49 +0100

    Reparo findById

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Importar paquetes con middlewares
 var express = require('express');
-var favicon = require('serve-favicon');
 var path = require('path');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
--- models/models.js
+++ models/models.js
@@ -4,14 +4,32 @@ var path = require('path');
 var Sequelize = require('sequelize');
 
 // Usar BBDD SQLite:
-var sequelize = new Sequelize(
+/*var sequelize = new Sequelize(
     null, //DB_NAME,
     null, //USERNAME,  
     null, //PASSWORD,
     { dialect: "sqlite", 
       storage: "quiz.sqlite"
     }
-);
+);*/
+
+// Para usar en local BBDD SQLite:
+//    DATABASE_URL = sqlite:///
+//    DATABASE_STORAGE = quiz.sqlite
+// Para usar en Heroku BBDD Postgres:
+//    DATABASE_URL = postgres://user:passwd@host:port/database
+
+var url, storage;
+
+if (!process.env.DATABASE_URL) {
+    url = "sqlite:///";
+    storage = "quiz.sqlite";
+} else {
+    url = process.env.DATABASE_URL;
+    storage = process.env.DATABASE_STORAGE || "";
+}
+
+var sequelize = new Sequelize(url, {storage: storage});
 
 //Importar la definición de la tabla Quiz en quiz.js
 var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
